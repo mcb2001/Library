d04f3b2589a48fd9df4a7190c707d695fa0860c8
Marc Barnholdt <marc@barnholdt.net>
2023-02-21 11:54:46 +0100

    moving DI arround and adding auto ioptions

---
+++ Oc6.Library/Data/TsidIServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oc6.Library.Data
+{
+    public static class TsidIServiceCollectionExtensions
+    {
+        public static IServiceCollection AddITsidFactory(this IServiceCollection services)
+            => AddITsidFactory(services, ServiceLifetime.Singleton);
+
+        public static IServiceCollection AddITsidFactory(this IServiceCollection services, ServiceLifetime serviceLifetime)
+            => serviceLifetime switch
+            {
+                ServiceLifetime.Scoped => services.AddScoped<ITsidFactory, TsidFactory>(),
+                ServiceLifetime.Singleton => services.AddSingleton<ITsidFactory, TsidFactory>(),
+                ServiceLifetime.Transient => services.AddTransient<ITsidFactory, TsidFactory>(),
+                _ => throw new NotImplementedException(),
+            };
+    }
+}
